de80bd8163605ef7757c863b55a83bf44204bd92
amoblin@mpb <amoblin@gmail.com>
2013-10-24 13:43:30 +0800

    add missing gitconfig file.

---
+++ check_hard_link.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# author: amoblin <amoblin@gmail.com>
+# file name: check_hard_link.sh
+# create date: 2013-10-06 11:06:40
+# This file is created from ~/.marboo/source/media/file_init/default.init.sh
+# 本文件由 ~/.marboo/source/media/file_init/default.init.sh　复制而来
+
+ls -i _vimrc _zshrc _gitconfig _screenrc _bashrc _inputrc
+cd ~;ls -i .vimrc .zshrc .gitconfig .screenrc .bashrc .inputrc
