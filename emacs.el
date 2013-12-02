92ed9f6cf1c99f07b76ee6ac1afc8882828fb60f
amoblin@mpb <amoblin@gmail.com>
2013-12-02 19:44:25 +0800

    auto resume && evil

--- emacs.el
+++ emacs.el
@@ -7,10 +7,36 @@
 ;; 背景颜色
 (set-background-color "black")
 (set-foreground-color "white")
+;; 自动恢复
+(desktop-save-mode 1)
 
 ;; 显示行号
 (global-linum-mode 1)
 (setq linum-format "%d ")
+
+;; 设置备份目录
+;; Put autosave files (ie #foo#) in one place, *not*
+;; scattered all over the file system!
+(defvar autosave-dir
+ (concat "/tmp/emacs_autosaves/" (user-login-name) "/"))
+
+(make-directory autosave-dir t)
+
+(defun auto-save-file-name-p (filename)
+  (string-match "^#.*#$" (file-name-nondirectory filename)))
+
+(defun make-auto-save-file-name ()
+  (concat autosave-dir
+   (if buffer-file-name
+      (concat "#" (file-name-nondirectory buffer-file-name) "#")
+    (expand-file-name
+     (concat "#%" (buffer-name) "#")))))
+
+;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
+;; list contains regexp=>directory mappings; filenames matching a regexp are
+;; backed up in the corresponding directory. Emacs will mkdir it if necessary.)
+(defvar backup-dir (concat "/tmp/emacs_backups/" (user-login-name) "/"))
+(setq backup-directory-alist (list (cons "." backup-dir)))
 
 ;; 字体和编码
 ;; 设置英文字体
@@ -42,3 +68,20 @@
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
+;; evil
+(add-to-list 'load-path "~/.emacs.d/evil")
+(require 'evil)
+(evil-mode 1)
+
+(setq evil-default-state 'emacs)
+;这个是打开文件后默认进入emacs模式
+;我还要在emacs和vim模式里面切换：C-z，换来换去啊
+
+(define-key evil-emacs-state-map (kbd "C-o") 'evil-execute-in-normal-state)
+; C-o按键调用vim功能（就是临时进入normal模式，然后自动回来）
+; 比如，你要到第一行，可以使用emacs的 M-<，也可以使用evil的C-o gg
+; 其中C-o是进入vim模式 gg是去第一行，命令之后自动还原emacs模式！
+; "Fuck you!" 如何删除""里面的内容呢？Emacs的话，默认文本对象能力不强
+; 有了evil的拓展 C-o di" 轻轻松松搞定~
+; 比如C-o 3dd C-o dib C-o yy C-o p C-o f * 舒服啊~发挥想象力吧
