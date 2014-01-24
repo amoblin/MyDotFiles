4048974b76ce7eb6a830271f417fce98f924bab7
amoblin@db-sf <amoblin@gmail.com>
2014-01-23 19:05:56 -0500

    fit for CentOS 6.5

--- emacs.el
+++ emacs.el
@@ -41,10 +41,10 @@
 
 ;; Jade Mode
 (add-to-list 'load-path "~/.emacs.d/vendor/jade-mode")
-(require 'sws-mode)
-(require 'jade-mode)    
-(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
-(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
+;;(require 'sws-mode)
+;;(require 'jade-mode)
+;;(add-to-list 'auto-mode-alist '("\\.styl$" . sws-mode))
+;;(add-to-list 'auto-mode-alist '("\\.jade$" . jade-mode))
 
 ;; Adaptive Filling
 (setq adaptive-fill-regexp "[ \t]+\\|[ \t]*\\([0-9]+\\.\\|\\*+\\)[ \t]*")
