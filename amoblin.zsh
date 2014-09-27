69520123ee1f000f4b3bb329de0f7509f7847689
amoblin@mbp <amoblin@gmail.com>
2014-09-27 22:51:59 +0800

    * add tmux conf * add oh-my-zsh custom conf

---
+++ amoblin.zsh
@@ -0,0 +1,22 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+ZSH_THEME="amoblin"
+export EDITOR=emacs
+plugins=(git, tmux)
+
+#tmux_init()
+#{
+#    tmux new-session -s "amoblin" -d -n "~/Marboo"    # 开启一个会话
+#    tmux new-window -n "other"          # 开启一个窗口
+#    tmux split-window -h                # 开启一个竖屏
+#    tmux split-window -v "top"          # 开启一个横屏,并执行top命令
+#    tmux -2 attach-session -d           # tmux -2强制启用256color，连接已开启的tmux
+#}
+
+# 判断是否已有开启的tmux会话，没有则开启
+#if which tmux 2>&1 >/dev/null; then
+#    test -z "$TMUX" && (tmux attach || tmux_init)
+#fi
