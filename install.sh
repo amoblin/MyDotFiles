7c77a8a1ad42fc179cf92712f114cf850cb87d3f
amoblin@mbp <amoblin@gmail.com>
2020-02-25 12:47:03 +0800

    update install.sh for plantumlrc

--- install.sh
+++ install.sh
@@ -37,6 +37,10 @@ done
 item=`pwd`/emacsclient/EmacsClient.app
 d=/Applications/EmacsClient.app
 [ -s "$d" ] && echo "ignore " $item || (echo $item -\> $d; ln -sf $item $d)
+
+item=`pwd`/home_dot_files/_plantumlrc
+d=/usr/local/etc/plantumlrc
+[ -s "$d" ] && echo "ignore " $item || (echo $item -\> $d; ln -sf $item $d)
 exit
 
 #mkdir -p ~/.vim/backup
